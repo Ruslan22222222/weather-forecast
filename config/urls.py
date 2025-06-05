30ea6414424162abbd8f973c60857c97886d3116
Ruslan Malofeev <mark.malofeev2222@gmail.com>
2025-06-05 17:50:34 +0600

    added authentification via GOOGLE

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', include('weather.urls')),
+    path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='weather-config/auth/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
